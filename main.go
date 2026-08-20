70a0db154275c20391d9405e78deb2f76f318a99
EugenioBarbieriViale <eugenio.barbieri.viale@gmail.com>
2026-08-21 00:35:16 +0200

    post puzzle to mastodon

--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
+	"log"
 	"os"
-	// "github.com/mattn/go-mastodon"
+	"strings"
+
+	"github.com/joho/godotenv"
+	"github.com/mattn/go-mastodon"
 )
 
 type Puzzles struct {
@@ -35,11 +40,67 @@ func parse_puzzles(filename string) Puzzles {
 	return puzzles
 }
 
+func get_config_from_env() mastodon.Config {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("error loading .env file")
+	}
+
+	cfg := mastodon.Config{
+		Server: "https://mathstodon.xyz",
+	}
+
+	cfg.ClientID = os.Getenv("CLIENT_KEY")
+	cfg.ClientSecret = os.Getenv("CLIENT_SECRET")
+	cfg.AccessToken = os.Getenv("ACCESS_TOKEN")
+
+	return cfg
+}
+
+func create_toot(p Puzzle) string {
+	lines := strings.Split(p.Latex, "\n")
+	var clean_latex []string
+	for _, l := range lines {
+		l = strings.TrimSpace(l)
+		if l != "" {
+			clean_latex = append(clean_latex, l)
+		}
+	}
+
+	body := strings.Join(clean_latex, " \\\\\n")
+	body = strings.ReplaceAll(body, `\[3pt]`, "")
+
+	var latex strings.Builder
+	latex.WriteString("\\[\n")
+	latex.WriteString("\\begin{array}{c}\n")
+	latex.WriteString(body)
+	latex.WriteString("\n\\end{array}\n")
+	latex.WriteString("\\]")
+
+	diff := strings.ToLower(p.Difficulty)
+	toot := fmt.Sprintf("%s (day no. %d)\nDifficulty: %s\n\n%s\n\nHave fun!", p.Title, p.Day, diff, latex.String())
+
+	return toot
+}
+
 func main() {
 	filename := "./data/dailyintegral.json"
 	puzzles := parse_puzzles(filename)
 
-	for _, puzzle := range puzzles.Content {
-		fmt.Println("diff:", puzzle.Difficulty)
+	// for _, puzzle := range puzzles.Content {
+	// 	fmt.Println("diff:", puzzle.Difficulty)
+	// }
+
+	cfg := get_config_from_env()
+	c := mastodon.NewClient(&cfg)
+
+	_, err := c.PostStatus(context.Background(), &mastodon.Toot{
+		Status: create_toot(puzzles.Content[0]),
+	})
+
+	if err != nil {
+		log.Fatal(err)
 	}
+
+	log.Println("toot posted!")
 }
